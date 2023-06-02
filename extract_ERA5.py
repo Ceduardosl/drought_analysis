a81c1028d6a6edc8a3686c9545615ec5e4029536
Eduardo Lima <ce-lima@hotmail.com>
2023-06-02 16:32:17 -0300

    Extração ERA5 Er

--- extract_ERA5.py
+++ extract_ERA5.py
@@ -1,5 +1,7 @@
 #%%
-#Extract multiple CRU data from multiple shapefiles
+#Extract multiple ERA5 total evaporation
+#negative values indicate evaporation.
+#positive values indicate condensation.
 __author__ = "Carlos Eduardo Sousa Lima"
 __license__ = "GPL"
 __version__ = "2.0"
@@ -16,35 +18,50 @@ from glob import glob
 
 list_nc = glob("Dados/ERA5_data/*.nc")
 
-
-dir_nc = list_nc[0]
-
-nc_data = xr.open_dataset(dir_nc)
-
-var = list(nc_data.data_vars)[0]
 #%%
-lon, lat = np.meshgrid(nc_data["lon"], nc_data["lat"])
-df_point = pd.DataFrame({"lon": lon.flatten(), "lat": lat.flatten()})
-shp_point = gpd.GeoDataFrame(df_point, geometry = gpd.points_from_xy(df_point["lon"], df_point["lat"], crs="EPSG:4326"))
+for dir_nc in list_nc:
 
-basins = glob("Shapes/Basins/*.shp")
+    nc_data = xr.open_dataset(dir_nc)
+    nc_data.coords["longitude"] = (nc_data.coords["longitude"] + 180) % 360 - 180
 
-for basin in basins:
-    shp_basin = gpd.read_file(basin)
+    var = list(nc_data.data_vars)[0]
+    start_time, end_time = (nc_data.time[0].values, nc_data.time[-1].values)
 
-    gdf_result = gpd.sjoin(shp_point, gpd.GeoDataFrame(geometry = shp_basin.envelope), how = "left")
-    gdf_result = gdf_result.loc[~np.isnan(gdf_result["index_right"])]
-    ins_point = gdf_result[["lon", "lat"]].values[:]
-    ins_df = pd.DataFrame(ins_point, columns = ["lon", "lat"])
+    lon, lat = np.meshgrid(nc_data["longitude"], nc_data["latitude"])
+    df_point = pd.DataFrame({"lon": lon.flatten(), "lat": lat.flatten()})
+    gdf_point = gpd.GeoDataFrame(df_point, geometry = gpd.points_from_xy(df_point["lon"], df_point["lat"], crs="EPSG:4326"))
 
-    var_ins = []
+    basins = glob("Shapes/Basins/*.shp")
 
-    for i in range(len(ins_df)):
-        var_ins.append(nc_data[var].sel(lat = ins_df["lat"][i], lon = ins_df["lon"][i]).values)
+    for basin in basins:
 
-    var_df = ins_df[["lon", "lat"]].join(pd.DataFrame(var_ins, columns = nc_data["time"]))
-    var_df.to_csv("Dados/Extracted_data/{}_{}_{}_{}.csv".format(
-        basin.split("\\")[-1].split(".")[0],
-        var,
-        nc_data.time_coverage_start,
-        nc_data.time_coverage_end), sep = ";", index = None, header = True)
+        shp_basin = gpd.read_file(basin)
+
+        gdf_result = gpd.sjoin(gdf_point, gpd.GeoDataFrame(geometry = shp_basin.geometry), how = "left")
+        gdf_result = gdf_result.loc[~np.isnan(gdf_result["index_right"])]
+        ins_point = gdf_result[["lon", "lat"]].values[:]
+        ins_df = pd.DataFrame(ins_point, columns = ["lon", "lat"])
+
+        var_ins = []
+
+        for i in range(len(ins_df)):
+            var_ins.append(nc_data[var].sel(latitude = ins_df["lat"][i], longitude = ins_df["lon"][i]).values)
+
+        var_df = ins_df[["lon", "lat"]].join(pd.DataFrame(var_ins, columns = nc_data["time"]))
+        
+    if dir_nc == list_nc[0]:
+        merged_df = var_df
+    else:
+        merged_df = merged_df.merge(var_df, how = "inner", on=("lon", "lat"))
+    
+    merged_df.to_csv("Dados/Extracted_data/Er_{}.csv".format(
+    basin.split("\\")[-1].split(".")[0]),
+    sep = ";", index = None, header = True)
+# %%
+mean_df = merged_df.mean(axis = 0)
+mean_df.drop(["lon", "lat"], axis = 0, inplace = True)
+mean_df.name = "Er (m)"
+mean_df.to_csv("Dados/Extracted_data/Mean_Er_{}.csv".format(
+    basin.split("\\")[-1].split(".")[0]),
+    sep = ";", index = True, header = True)
+# %%
